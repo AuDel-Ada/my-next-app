85e5921aa739f84ce4a03a06dd5d069101ce3ba1
Aurelie D <adelaunay@palo-it.com>
2024-01-29 16:15:19 +0100

    server and client components

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -2,9 +2,20 @@
 //rafce to begin the page easily
 
 import React from 'react';
+import ProductCard from '../components/ProductCard';
 
 const UsersPage = () => {
-  return <div>Hello You !</div>;
+  return (
+    <div>
+      <h2 className={`mb-3 text-2xl font-semibold`}>Hello You !</h2>
+      <div className="flex flex-row gap-2">
+        <span className="transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+          -&gt;
+        </span>
+        <ProductCard />
+      </div>
+    </div>
+  );
 };
 
 export default UsersPage;
