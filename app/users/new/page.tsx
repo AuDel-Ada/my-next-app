ed44995dc017095e4fe0143c97fdf54fd1687592
Aurelie D <adelaunay@palo-it.com>
2024-01-29 15:49:27 +0100

    news pages

---
+++ app/users/new/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NewUserPage = () => {
+  return <div>Hello Newby !</div>;
+};
+
+export default NewUserPage;
---
+++ app/users/page.tsx
@@ -0,0 +1,10 @@
+//routing by folder, rendering the page.tsx
+//rafce to begin the page easily
+
+import React from 'react';
+
+const UsersPage = () => {
+  return <div>Hello You !</div>;
+};
+
+export default UsersPage;
