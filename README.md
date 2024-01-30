ed85373aa69eebf8a0fc4fbf866cea5e15bdeae9
Aurelie D <adelaunay@palo-it.com>
2024-01-30 17:33:38 +0100

    personnalized app goal and styles

--- README.md
+++ README.md
@@ -1,22 +1,16 @@
+# My Vacation App
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+
+Styling by Tailwind
+
+Based on tuto https://www.youtube.com/watch?v=ZVnjOPwW4ZA
+
+As it is my first NextJS app, I am mixing comments and basic tricks.
 
 ## Getting Started
 
-First, run the development server:
-
-```bash
 npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-```
-
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
 
 ## Learn More
 
---
+++ app/ponts/page.tsx
@@ -0,0 +1,41 @@
+//routing by folder, rendering the page.tsx
+//rafce to begin the page easily
+
+import React from 'react';
+import ProductCard from '../components/ProductCard';
+import './page.css';
+
+interface Photo {
+  id: number;
+  title: string;
+  url: string;
+}
+
+const UsersPage = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/photos');
+  const photos: Photo[] = await res.json();
+  return (
+    <div className="background">
+      <h1 className={`mb-3 text-2xl font-semibold`}>Avignon Baby !</h1>
+
+      <div className="rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+        <div className="flex gap-2">
+          <span className="transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            -&gt;
+          </span>
+          <ProductCard />
+        </div>
+      </div>
+
+      <ul>
+        {photos.slice(0, 5).map((photo) => (
+          <li key={photo.id}>
+            {photo.title}: <a href={photo.url}>{photo.url}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UsersPage;
